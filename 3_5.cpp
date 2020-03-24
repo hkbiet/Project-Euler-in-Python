2681460b24624651b7e45d23c9131ab50c259e65
Hemant Kumar <hkbiet@gmail.com>
2020-03-24 11:28:11 +0530

    added

---
+++ 3_5.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+	int input;
+	cout<<"\nENTER THY NUMBER\n";
+	cin>>input;
+	int sum = 0;
+
+	while(input){
+	
+		if(input%3==0){
+			sum = sum + input;
+		
+		}else if(input%5==0){
+		
+			sum = sum + input;
+		}
+	
+	
+	input--;
+	}
+
+	cout<<sum<<endl;
+
+
+
+
+return 0;
+}
---
+++ even.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+// 4 million Fibonacci numbers
+
+int main(){
+	
+	/*
+        int a = 0;
+        int b = 1;
+        int c  = a + b;
+	*/
+
+	
+	long long a = 0;
+	long long b = 1;
+	long long c  = a + b;
+	
+	long sum = 0;
+
+	//long long  n = 4000000;
+	while(c<=4000000){
+	
+
+
+		if(c%2 == 0){
+		
+			sum = sum + c;
+		
+		}
+		cout<<c<<endl;
+		a=b;
+		b=c;
+		c=a+b;
+		
+
+		
+	}
+
+	cout<<"sum = "<<sum<<endl;
+
+return 0;
+}
